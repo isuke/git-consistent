d7f5c4838973e3c77026524e675f9f82fa1e54cd
isuke <isuke770@gmail.com>
2019-04-26 13:56:52 +0900

    tada: update to v0.9.8

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ const test = require('ava')
 const execSync = require('child_process').execSync
 const path = require('path')
 
-const version = '0.9.7'
+const version = '0.9.8'
 
 test('--version', (t) => {
   const output = execSync(`${path.join(__dirname, '../lib/index.js')} --version`)
