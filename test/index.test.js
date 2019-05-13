7bea24429a69b15431d96f8e161e962b1f7097df
isuke <isuke770@gmail.com>
2019-05-13 21:14:28 +0900

    refactor: make to get version from package.json

--- test/index.test.js
+++ test/index.test.js
@@ -2,12 +2,10 @@ const test = require('ava')
 const execSync = require('child_process').execSync
 const path = require('path')
 
-const version = '0.9.8'
-
 test('--version', (t) => {
   const output = execSync(`${path.join(__dirname, '../lib/index.js')} --version`)
     .toString()
     .trim()
 
-  t.is(output, version)
+  t.is(output, '0.9.8')
 })
